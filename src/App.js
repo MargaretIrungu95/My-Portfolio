6b7d8f4d13ec449a8c8e6ba66a7775eb70d3ada2
Margaret Irungu <mwirungu22@gmail.com>
2023-11-07 20:35:14 +0000

    Imported the css and components into the respective pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element= {<Home/>} />
+          <Route path='/about' element= {<About/>}/>
+          <Route path='/skills' element= {<Skills/>}/>
+          <Route path='/projects' element= {<Projects/>}/>
+          <Route path='/contact' element= {<Contact/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
