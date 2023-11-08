11ee02d39bb31b2caee12e2682b4f8fa14dc1fd1
Margaret Irungu <mwirungu22@gmail.com>
2023-11-08 15:34:49 +0000

    Basic styling and content for Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import logo from '../Images/logo.png';
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='nav-wrapper'>
+      <nav>      
+        <img className='logo' src= {logo} alt='logo'/>
+        <p className='name'>Margaret Irungu
+          <span> |Software Developer</span>
+        </p>
+  
+        <Link to= '/about' style={{color: 'white'}}>
+          About me
+        </Link>
+
+        <Link to= '/projects' style={{color: 'white'}}>
+          Projects
+        </Link>
+
+        <Link to= '/contact' style={{color: 'white'}}>
+          Contact me
+        </Link>
+
+        <Link to= '/skills' style={{color: 'white'}}>
+          Skills
+        </Link>
+      </nav>
+    </div>
   )
 }
 
